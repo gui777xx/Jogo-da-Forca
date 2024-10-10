4ac4153f92a53a6a461370b3f5a67800e3da3df9
gui777x <guizin.clementino@gmail.com>
2024-10-10 12:26:39 -0300

    feito

---
+++ main.js
@@ -0,0 +1,5 @@
+const palavras = ["casaoito", "drogas", "capitalfalido"];
+let tentativasUsadas = 0;
+let letras = ['A','B','C','D','E','F','G','H','V','I','V','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let palavraSecreta, palavraOculta;
+let jogando
